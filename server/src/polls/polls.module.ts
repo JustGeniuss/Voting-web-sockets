2bec68a2953d65d68e14a23fee7b38562bfd2369
JustGeniuss <danila.kukushkin98@gmail.com>
2023-04-03 14:15:26 +0300

    added Redis Module

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,10 +2,11 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { redisModule } from 'src/modules.config';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, redisModule],
   providers: [PollsService],
-  controllers: [PollsController],
+  controllers: [PollsController]
 })
 export class PollsModule {}
