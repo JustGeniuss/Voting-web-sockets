cec2ba0f79ec97715ea974a14861a74d40210262
JustGeniuss <danila.kukushkin98@gmail.com>
2023-04-04 15:21:15 +0300

    add shared workspace

---
+++ shared/poll-types.ts
@@ -0,0 +1,15 @@
+export interface Participants {
+    [participantId: string]: string;
+}
+
+export interface Poll {
+    id: string;
+    topic: string;
+    votesPerVoter: number;
+    participants: Participants;
+    adminID: string;
+    // nominations: Nominations;
+    // reanking: Rankings;
+    // results: Results;
+    // hasStarted: boolean;
+}
