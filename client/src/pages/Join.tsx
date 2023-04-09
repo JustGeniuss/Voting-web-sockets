6265c43837df00c8903025142771b97febeb4be5
JustGeniuss <danila.kukushkin98@gmail.com>
2023-04-09 23:28:24 +0300

    added pageState

---
+++ client/src/pages/Join.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Create: React.FC = () => {
+    return <div className="flex flex-col w-full justify-around items-stretch h-full mx-auto max-w-sm">
+        <h3 className="text-center">Enter Code Provided by &quot;Friend&quot;</h3>
+    </div>
+}
+
+export default Create; 
